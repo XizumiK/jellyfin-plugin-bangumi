870c1c7e50ba449b02b77869f08ac7795f19c410
izumi <izumiKawaDT@outlook.com>
2024-04-23 14:32:43 +0800

    Init

---
+++ Jellyfin.Plugin.Bangumi/Provider/Bangumi/BangumiApi.cs
@@ -0,0 +1,14 @@
+namespace Jellyfin.Plugin.Bangumi.Provider.Bangumi
+{
+    public class BangumiApi
+    {
+        private const string SearchLink = @""
+
+        private const string SubjectLink = @""
+
+        public async Task<Media> GetSubject(string id)
+        {
+            
+        }
+    }
+}
